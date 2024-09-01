fe4827561e82bdc88a696c7300632e3dbc97b16f
rayen oueslati <rayenoueslati60@gmail.com>
2024-09-01 02:20:00 +0100

    feat : improve calendar

--- frontend/src/components/calendar/MyCalendar.jsx
+++ frontend/src/components/calendar/MyCalendar.jsx
@@ -54,67 +54,76 @@ const MyCalendar = () => {
   const handleYearSelect = (year) => {
     setSelectedYear(year);
     const calendarApi = calendarRef.current.getApi();
-    calendarApi.gotoDate(`${year}-01-01`);
+    const currentMonth = (new Date().getMonth() + 1).toString().padStart(2, '0');
+    const currentDay = new Date().getDate().toString().padStart(2, '0');
+    if (year == new Date().getFullYear()) {
+      calendarApi.gotoDate(`${year}-${currentMonth}-${currentDay}`);
+    } else {
+      calendarApi.gotoDate(`${year}-01-01`);
+    }
   };
 
   return (
     <div className="calendar-container">
-  <div className="date-picker-container">
-    <div>
-      <label htmlFor="start">Start Date: </label>
-      <input
-        type="date"
-        id="start"
-        name="trip-start"
-        value={startDate}
-        min={minDate}
-        max={maxDate}
-        onChange={handleStartDateChange}
-      />
+      <div className="date-picker-container">
+        <div>
+          <label htmlFor="start">Start Date: </label>
+          <input
+            type="date"
+            id="start"
+            name="trip-start"
+            value={startDate}
+            min={minDate}
+            max={maxDate}
+            onChange={handleStartDateChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="end">End Date: </label>
+          <input
+            type="date"
+            id="end"
+            name="trip-end"
+            value={endDate}
+            min={startDate || minDate}
+            max={maxDate}
+            onChange={handleEndDateChange}
+          />
+        </div>
+        <button
+          className="export-btn"
+          onClick={exportToExcel}
+          disabled={!exportEnabled}
+        >
+          Export to Excel
+        </button>
+      </div>
+      <div className="dropdown-years">
+        <DropdownYears selectedYear={selectedYear} onYearChange={handleYearSelect} />
+      </div>
+      <div className='calendar-card'>
+        <FullCalendar
+          ref={calendarRef}
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          initialView="dayGridMonth"
+          selectable
+          dateClick={(info) => {
+            if (!startDate) {
+              handleStartDateChange({ target: { value: info.dateStr } });
+            } else {
+              handleEndDateChange({ target: { value: info.dateStr } });
+            }
+          }}
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: '',
+          }}
+          style={{ height: '450px', width: '100%', margin: '0 auto' }}
+        />
+      </div>
+
     </div>
-    <div>
-      <label htmlFor="end">End Date: </label>
-      <input
-        type="date"
-        id="end"
-        name="trip-end"
-        value={endDate}
-        min={startDate || minDate}
-        max={maxDate}
-        onChange={handleEndDateChange}
-      />
-    </div>
-    <button
-      className="export-btn"
-      onClick={exportToExcel}
-      disabled={!exportEnabled}
-    >
-      Export to Excel
-    </button>
-  </div>
-  <div className="dropdown-years">
-    <DropdownYears selectedYear={selectedYear} onYearChange={handleYearSelect} />
-  </div>
-  <FullCalendar
-    ref={calendarRef}
-    plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-    initialView="dayGridMonth"
-    selectable
-    dateClick={(info) => {
-      if (!startDate) {
-        handleStartDateChange({ target: { value: info.dateStr } });
-      } else {
-        handleEndDateChange({ target: { value: info.dateStr } });
-      }
-    }}
-    headerToolbar={{
-      left: 'prev,next today',
-      center: 'title',
-      right: '',
-    }}
-    style={{ height: '450px', width: '100%' }}
-  />
-</div>
   )
 };
 
